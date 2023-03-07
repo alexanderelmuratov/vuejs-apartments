b3679224fdf3451e0d0aa42f54f8e4b791d24d2c
alexanderelmuratov <elmuratov@gmail.com>
2023-03-07 18:43:59 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuejs-apartments/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/vuejs-apartments/'
+  //   : '/'
 })
+
+
+module.exports = {
+publicPath: "/vuejs-apartments/",
+}
